10c8e4d879ff964d53ee85fd89d39f4044da6e0c
JW Nicholson <john.nicholson812@gmail.com>
2020-04-20 15:30:47 -0400

    basic file structure completed. Server is running on port 6000

---
+++ api/server.js
@@ -0,0 +1,13 @@
+express = require('express');
+
+//const apiRouter = require('./api-router');
+
+const server = express();
+
+//server.use('api', apiRouter);
+
+server.get('/', (req,res) => {
+    res.send('<h1>Welcome to the Node Auth1 Project</h1>')
+});
+
+module.exports = server;
