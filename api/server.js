7590408acf789aa8f3ceb1f359c615b1ccde4315
JW Nicholson <john.nicholson812@gmail.com>
2020-04-20 16:20:46 -0400

    auth-routes and restricted-middleware done

--- api/server.js
+++ api/server.js
@@ -1,10 +1,10 @@
 express = require('express');
 
-//const apiRouter = require('./api-router');
+const apiRouter = require('./api-router');
 
 const server = express();
 
-//server.use('api', apiRouter);
+server.use('api', apiRouter);
 
 server.get('/', (req,res) => {
     res.send('<h1>Welcome to the Node Auth1 Project</h1>')
